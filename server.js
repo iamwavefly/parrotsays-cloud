654dfeae613cc3e94de5c644ca5f141dfb92895b
wavefly <iamwavefly@gmail.com>
2021-05-18 20:01:07 +0100

    removed new

--- server.js
+++ server.js
@@ -96,22 +96,6 @@ app.post('/start', async (req, res) => {
             backgroundImage: 'https://i.ibb.co/98y9ZSN/parrotsays.jpg',
           },
         },
-        // extensionServiceConfig: {
-        //   errorHandlePolicy: 'error_abort',
-        //   extensionServices: [
-        //     {
-        //       serviceName: 'web_recorder_service',
-        //       errorHandlePolicy: 'error_abort',
-        //       serviceParam: {
-        //         url: 'http://localhost:3000/?user_id=test&username=testhttp://localhost:3000/',
-        //         audioProfile: 0,
-        //         videoWidth: 1280,
-        //         videoHeight: 720,
-        //         maxRecordingHour: 72,
-        //       },
-        //     },
-        //   ],
-        // },
         recordingFileConfig: {
           avFileType: ['hls', 'mp4'],
         },
@@ -125,26 +109,15 @@ app.post('/start', async (req, res) => {
         },
       },
     },
-    { headers: { Authorization } }
+    { headers: { Authorization, 'Content-Type': 'application/json' } }
   );
 
   res.send(start.data);
 });
-
-app.post('/query', async (req, res) => {
-  const appID = AppId;
-  const resource = req.body.resource;
-  const sid = req.body.sid;
-  const mode = req.body.mode;
-
-  const query = await axios.get(
-    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/query`,
-    { headers: { Authorization } }
-  );
-  res.send(query.data);
-});
-
 app.post('/stop', async (req, res) => {
+  const Authorization = `Basic ${Buffer.from(
+    `${customerKey}:${customerSecret}`
+  ).toString('base64')}`;
   const appID = AppId;
   const resource = req.body.resource;
   const sid = req.body.sid;
@@ -161,57 +134,17 @@ app.post('/stop', async (req, res) => {
   );
   res.send(stop.data);
 });
-
-app.post('/updateSubscription', async (req, res) => {
+app.post('/query', async (req, res) => {
   const appID = AppId;
   const resource = req.body.resource;
   const sid = req.body.sid;
   const mode = req.body.mode;
 
-  const updateSubscription = await axios.get(
-    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/update`,
-    {
-      cname: req.body.channel,
-      uid: req.body.uid,
-      clientRequest: {
-        streamSubscribe: {
-          audioUidList: req.body.audioSubscription,
-          videoUidList: req.body.videoSubscription,
-        },
-      },
-    },
+  const query = await axios.get(
+    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/query`,
     { headers: { Authorization } }
   );
-
-  res.send(updateSubscription.data);
-});
-
-app.post('/updateLayout', async (req, res) => {
-  const appID = AppId;
-  const resource = req.body.resource;
-  const sid = req.body.sid;
-  const mode = req.body.mode;
-
-  const body = {
-    cname: req.body.channel,
-    uid: req.body.uid,
-    clientRequest: {
-      mixedVideoLayout: req.body.layout,
-      backgroundColor: req.body.backgroundColor,
-    },
-  };
-
-  if (req.body.layoutConfig === 3) {
-    body.clientRequest.layoutConfig = req.body.layoutConfig;
-  }
-
-  const updateLayoutConfig = await axios.get(
-    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/updateLayout`,
-    body,
-    { headers: { Authorization } }
-  );
-
-  res.send(updateLayoutConfig.data);
+  res.send(query.data);
 });
 
 const port = process.env.PORT || 5000;
